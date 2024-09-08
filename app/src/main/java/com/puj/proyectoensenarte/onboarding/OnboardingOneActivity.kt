47272ed519ff475b50489528a01929021a18064d
DiegoPardoMontero <di-pardo@javeriana.edu.co>
2024-09-08 02:26:03 -0500

    Add auth screens and navegation

--- app/src/main/java/com/puj/proyectoensenarte/onboarding/OnboardingOneActivity.kt
+++ app/src/main/java/com/puj/proyectoensenarte/onboarding/OnboardingOneActivity.kt
@@ -1,11 +1,14 @@
 package com.puj.proyectoensenarte.onboarding
 
 //import android.content.Intent
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.puj.proyectoensenarte.CrearCuentaActivity
+import com.puj.proyectoensenarte.IniciarSesionActivity
 import com.puj.proyectoensenarte.databinding.ActivityOnboardingOneBinding
 
 class OnboardingOneActivity : Fragment() {
@@ -19,13 +22,13 @@ class OnboardingOneActivity : Fragment() {
         _binding = ActivityOnboardingOneBinding.inflate(inflater, container, false)
 
         binding.botonCrearCuenta1.setOnClickListener {
-            //val intent = Intent(activity, CrearCuentaActivity::class.java)
-            //startActivity(intent)
+            val intent = Intent(activity, CrearCuentaActivity::class.java)
+            startActivity(intent)
         }
 
         binding.botonIniciarSesion1.setOnClickListener {
-            //val intent = Intent(activity, IniciarSesionActivity::class.java)
-            //startActivity(intent)
+            val intent = Intent(activity, IniciarSesionActivity::class.java)
+            startActivity(intent)
         }
 
         return binding.root
