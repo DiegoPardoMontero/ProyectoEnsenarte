36c7b94b6ff7f6746157d07cb98d5585816ef5c4
ValEscoSierra <svalentinasierra@javeriana.edu.co>
2024-09-08 13:12:08 -0500

    created bottom navigation activity and fragments

--- app/src/main/java/com/puj/proyectoensenarte/onboarding/OnboardingOneActivity.kt
+++ app/src/main/java/com/puj/proyectoensenarte/onboarding/OnboardingOneActivity.kt
@@ -1,11 +1,13 @@
 package com.puj.proyectoensenarte.onboarding
 
-//import android.content.Intent
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.puj.proyectoensenarte.BottomNavigationActivity
 import com.puj.proyectoensenarte.databinding.ActivityOnboardingOneBinding
 
 class OnboardingOneActivity : Fragment() {
@@ -19,8 +21,8 @@ class OnboardingOneActivity : Fragment() {
         _binding = ActivityOnboardingOneBinding.inflate(inflater, container, false)
 
         binding.botonCrearCuenta1.setOnClickListener {
-            //val intent = Intent(activity, CrearCuentaActivity::class.java)
-            //startActivity(intent)
+            val intent = Intent(activity, BottomNavigationActivity::class.java)
+            startActivity(intent)
         }
 
         binding.botonIniciarSesion1.setOnClickListener {
