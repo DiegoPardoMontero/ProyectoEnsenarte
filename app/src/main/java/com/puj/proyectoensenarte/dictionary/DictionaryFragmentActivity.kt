a79ab73a3d50b4c782fb35be27fe0ccdbc445203
ValEscoSierra <svalentinasierra@javeriana.edu.co>
2024-09-08 13:15:23 -0500

    Fixed Dictionary Fragment in package dictionary

--- app/src/main/java/com/puj/proyectoensenarte/dictionary/DictionaryFragmentActivity.kt
+++ app/src/main/java/com/puj/proyectoensenarte/dictionary/DictionaryFragmentActivity.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.puj.proyectoensenarte.R
 
-class DiccionarioFragmentActivity : Fragment() {
+class DictionaryFragmentActivity : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
