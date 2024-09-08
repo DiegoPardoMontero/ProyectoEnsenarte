1f40a0c87aa949a37d5810e6b0b0cf15bbba7d33
ValEscoSierra <svalentinasierra@javeriana.edu.co>
2024-09-08 01:06:22 -0500

    res added and onboarding feature completed

---
+++ app/src/main/java/com/puj/proyectoensenarte/onboarding/OnboardingOneActivity.kt
@@ -0,0 +1,38 @@
+package com.puj.proyectoensenarte.onboarding
+
+//import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.puj.proyectoensenarte.databinding.ActivityOnboardingOneBinding
+
+class OnboardingOneActivity : Fragment() {
+    private var _binding: ActivityOnboardingOneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivityOnboardingOneBinding.inflate(inflater, container, false)
+
+        binding.botonCrearCuenta1.setOnClickListener {
+            //val intent = Intent(activity, CrearCuentaActivity::class.java)
+            //startActivity(intent)
+        }
+
+        binding.botonIniciarSesion1.setOnClickListener {
+            //val intent = Intent(activity, IniciarSesionActivity::class.java)
+            //startActivity(intent)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
