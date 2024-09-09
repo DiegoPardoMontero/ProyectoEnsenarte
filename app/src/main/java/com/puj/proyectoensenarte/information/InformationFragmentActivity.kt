06243eb28eabc2cd4388db22f961162cc982d097
jparrado <jparrado@idl.com.co>
2024-09-09 16:47:35 -0500

    Update: Creación de la pagina principal de information

--- app/src/main/java/com/puj/proyectoensenarte/information/InformationFragmentActivity.kt
+++ app/src/main/java/com/puj/proyectoensenarte/information/InformationFragmentActivity.kt
@@ -4,16 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.puj.proyectoensenarte.R
+import com.puj.proyectoensenarte.databinding.ActivityInformationFragmentBinding
 
 class InformationFragmentActivity : Fragment() {
+    private lateinit var binding: ActivityInformationFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_information_fragment, container, false)
+    ): View {
+        binding = ActivityInformationFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //LINKS BUTTON
+        binding.linksButton.setOnClickListener{
+            Toast.makeText(requireContext(), "Links seleccionado", Toast.LENGTH_SHORT).show()
+        }
+        //GRAMMAR BUTTON
+        binding.grammarButton.setOnClickListener{
+            Toast.makeText(requireContext(), "Grammar seleccionado", Toast.LENGTH_SHORT).show()
+        }
+        //COMMUNITY BUTTON
+        binding.communityButton.setOnClickListener{
+            Toast.makeText(requireContext(), "Community seleccionado", Toast.LENGTH_SHORT).show()
+        }
+        //HISTORY BUTTON
+        binding.historyButton.setOnClickListener{
+            Toast.makeText(requireContext(), "History seleccionado", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+
 }
