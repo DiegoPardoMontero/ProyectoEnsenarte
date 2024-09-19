eb4967ce85ae778e56687296619fb1e78cf9c45a
DiegoPardoMontero <di-pardo@javeriana.edu.co>
2024-09-18 22:02:48 -0500

    :bug: Fix errors in search bar

--- app/src/main/java/com/puj/proyectoensenarte/dictionary/ResultadoBusquedaCategoriaActivity.kt
+++ app/src/main/java/com/puj/proyectoensenarte/dictionary/ResultadoBusquedaCategoriaActivity.kt
@@ -1,8 +1,10 @@
 package com.puj.proyectoensenarte.dictionary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.storage.FirebaseStorage
 import com.puj.proyectoensenarte.databinding.ActivityResultadoBusquedaCategoriaBinding
 
 class ResultadoBusquedaCategoriaActivity : AppCompatActivity() {
@@ -21,7 +23,6 @@ class ResultadoBusquedaCategoriaActivity : AppCompatActivity() {
     }
 
     private fun setupSearchBar() {
-        binding.etSearch.setText(intent.getStringExtra("SEARCH_QUERY"))
         binding.ivSearch.setOnClickListener {
             performSearch(binding.etSearch.text.toString())
         }
@@ -30,15 +31,36 @@ class ResultadoBusquedaCategoriaActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         categoryAdapter = CategoryAdapter { category ->
             // Navegar a la pantalla de detalle de categoría
+            // Implementar la navegación aquí
         }
         binding.rvSearchResults.adapter = categoryAdapter
         binding.rvSearchResults.layoutManager = LinearLayoutManager(this)
     }
 
     private fun performSearch(query: String) {
-        val searchResults = listOf(
-            Category("url_imagen_1", "Vestuario"),
-        )
-        categoryAdapter.submitList(searchResults)
+        var query = capitalizeFirstLetter(query)
+        val storage = FirebaseStorage.getInstance()
+        val imageRef = storage.reference.child("imagenesCategorias/$query.png")
+
+        imageRef.downloadUrl.addOnSuccessListener { uri ->
+            val downloadUrl = uri.toString()
+            val formattedQuery = query.replace(Regex("(?<=.)([A-Z])"), " $1")
+            val category = Category(downloadUrl, formattedQuery)
+            categoryAdapter.submitList(listOf(category))
+        }.addOnFailureListener { exception ->
+            Log.e("ResultadoBusqueda", "Error al cargar la imagen de la categoría: ${exception.message}")
+            val category = Category("url_imagen_por_defecto", query)
+            categoryAdapter.submitList(listOf(category))
+        }
     }
+
+    private fun capitalizeFirstLetter(input: String): String {
+        return if (input.isNotEmpty()) {
+            input.substring(0, 1).uppercase() + input.substring(1).lowercase()
+        } else {
+            input
+        }
+    }
+
+
 }
